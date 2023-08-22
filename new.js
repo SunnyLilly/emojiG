c2ae543c6e266c8b94e6248cc2007e68d2a8e951
SunnyLilly <tomiwav52@gmail.com>
2023-08-22 12:43:26 +0100

    first commit

---
+++ new.js
@@ -0,0 +1,422 @@
+// [1,2,3,4,5,7]
+
+//
+// let numArrays =  [1,2,3,4,5,7];
+
+// + , -, /, * , % arithemetic operators
+
+/*create some variable and assigned the a number data type do some
+arithemetic operations 
+
+*/
+
+// >, <, <=, >=, ==,!=, === comparison operators
+
+/*create some variable and assigned the a number data type do some
+comparison operations 
+
+*/
+
+
+// let age = 2;
+// let myvar = "2";
+// let MyVar = 4;
+// let myVar = 2;
+
+// let result = (myvar + MyVar) * myVar;
+
+//console.log("hello" === "Hello");
+
+// let jan = 5;
+// let feb = 9;
+// let mar = 7;
+// let apr = 3;
+// let may = 5;
+// let jun = 1;
+// let jul = 7;
+// let aug = 8;
+// let sep = 4;
+// let oct = 10;
+
+// let outcome = jan + apr;
+
+// console.log(outcome);
+
+// let result = jan * oct;
+
+// console.log(result)
+
+// let reslt = may - mar;
+
+// console.log(reslt);
+
+// let bodmas = oct - jan + mar / aug;
+
+// console.log(bodmas);
+
+// console.log(jan == may);
+
+// console.log(jul < jun);
+
+// console.log(feb > mar);
+
+// console.log(oct <= sep);
+
+// console.log(apr >= jun);
+// && AND both of the condition must return true
+// ! Not return false when true and vice versa
+// || OR one of the condition must be true 
+//backtick
+
+// let newType = "Hello";
+
+// let mergeType = "Nfemi";
+
+// let value = newType + " " + mergeType;
+// let values = `${newType} ${mergeType}`;
+
+// console.log(values);
+
+// let age = 2010;
+
+// let year = 2010;
+
+// if (age != year ) {
+//     // perform task
+//     console.log("age is greater");
+// }else{
+//    console.log("age is not greater"); 
+// }
+
+// let red = 900;
+
+// let blue = 1000;
+
+// if (red != blue) {
+// console.log(red + " " + 'is greater');
+// }else{
+//     console.log(blue + " " + 'is not greater');
+// }
+
+
+// If-else estate
+// if(red > blue){
+
+//     console.log(red);
+
+// }else if(red === blue){
+    
+//     console.log(blue);
+
+// }else{
+    
+//     console.log("None is true");
+// }
+
+
+// let sun = 4000;
+
+// let moon = 4005;
+
+// if(moon > sun){ 
+//     console.log(moon);
+// }else if(moon > sun){
+//     console.log(sun);
+// }else{
+//     console.log("nothing");
+// }
+
+
+// Switch Statements
+// const day = "Monday";
+
+// switch (day) {
+//     case "Mondays":
+//         console.log("that is true");
+//         break;
+
+//     case "Tuesday":
+//         console.log("that is not true");
+//         break;
+
+//     default:
+//         console.log("none is there");
+//         break;
+// }
+
+// const seas = 123;
+
+// switch (seas) {
+// case 1234:
+//     console.log("this is true");
+// break;
+
+// case 156:
+//     console.log("this is not true");
+//     break;
+
+//     default:
+//         console.log("none");
+
+// }
+
+// Tenary Statment
+// condition ? value_if_true : value_if_false;
+//camelCase
+// let HerAge = 4;
+
+// let herAge = 4;
+
+// let message = HerAge <= herAge ? 'nothing' : 'something';
+
+// console.log(message);
+
+// let time = "spring";
+
+// let Time = "spring";
+
+// let final = time === Time ? 'yes' : 'no';
+
+// console.log(final);
+
+
+// looping
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// let count = 0; //initialization
+
+// while (count < 5) {
+//     console.log(count);
+//     count += 1; //increment
+
+
+// }
+
+
+
+// for loop
+
+// for (initialization; condition; increment/decrement) {
+    
+    
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// let array1 = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < array1.length; i++) {
+//     console.log(array1[i]);
+// }
+
+// let i = 0; //initialization
+// let counter = [1, 2, 3, 4, 5];
+
+// while (i < counter.length) {
+//     console.log(counter[i]);
+//     i += 1; //increment
+// }
+
+// let out = ["jan", "feb", "mar", "apr", "may"];
+
+// for (let f = 0; f < out.length; f++) {
+//     console.log(out[f]);
+// }
+
+// let count = 0;
+// do {
+//     console.log(count);
+//     count++;
+// } while (count < 7);
+
+// let bin = 0;
+// do {
+//     console.log(bin);
+//     bin++;
+// } while (bin < 8)
+
+
+// for (let index = 0; index < 5; index++) {
+//     if (index === 4) {
+//         break;
+//     }
+//     console.log(index);
+// }
+
+// for (let index = 0; index < 5; index++) {
+//     if (index === 2) {
+//         continue;
+//     }
+//     console.log(index);
+// }
+
+// for (look = 0; look < 5; look++) {
+//    if (look === 3) {
+//         continue;
+
+//    } 
+//    console.log(look);
+// }
+
+// for (look = 0; look < 5; look++) {
+//     if (look === 3) {
+//          break;
+ 
+//     } 
+//     console.log(look);
+//  }
+
+// function testing(name) {
+//     // code
+//     console.log(`hello ${name} !!`);
+// }
+
+// // calling function
+// testing("blessed");
+// testing("prince");
+
+// function test() {
+//     console.log("welcome");
+// }
+// test();
+
+// function right(place) {
+//     console.log("this is " + place);
+
+// }
+// right("lagos");
+
+
+// function add(x,y) {
+//     return x + y;
+
+// }
+
+// console.log(add(2, 6));
+
+
+// const sub = function(a, b) {
+//     return a - b;
+    
+// }
+// console.log(sub(10, 5));
+
+// let buttons = document.querySelector("button");
+
+//     buttons.addEventListener('click', function text() {
+//         buttons.textContent = 'click';
+//     });
+// text();
+
+// let array1 = [1, 2, 3, 4, 5];
+
+// console.log(array1[1]);
+
+// let array3 = new array3(1, 2, 3, 4);
+//  console.log(array3[]);
+// Array Methods (push, pop, shift, unshift, etc.)
+
+// array1.push("hello");
+// array1.pop();
+// array1.shift();
+// array1.unshift(4);
+// console.log(array1);
+// console.log(array1.length);
+
+// let string = ["a", "b", "c", "d", "e"];
+
+// console.log(string[1]);
+
+// const sring2 = new Array("1", "2", "3", "4");
+
+// console.log(sring2);
+
+// let arrayConcate = string.concat(sring2);
+
+// console.log(arrayConcate);
+
+// let hi = 1234
+
+// // arrayConcate.pop();
+// // arrayConcate.shift();
+// // arrayConcate.unshift(4);
+// arrayConcate.push(hi);
+// console.log(arrayConcate);
+
+// const obj = [
+//     ["1", "2", "3", "4", "5", "6", "7"],
+//     [1, 2, 3 , 4 , 5 , 6 , 7]
+// ]
+
+// console.log(typeof obj);
+//get Elements by Thier tag names
+// get Elements by their ID
+// get Elements by their Class name
+// query the type of elements either classes, id's or by thier tag names
+
+
+// function addNumbers(a,b){
+//     return a + b;
+// }
+
+// console.log(addNumbers(12,5));
+
+// class PocationType{
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// function PocationType(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
+
+// let person = new PocationType("John", 25);
+
+
+// person.prototype.PocationType = function() {
+//     console.log(`This is ${this.name} and I am ${this.age} years old.`);
+// };
+
+
+
+// console.log(ages);
+// let object1 = {
+//     name : "Prince",
+//     age : 13
+// }
+
+// console.log(object1.age);
+
+let button = document.querySelectorAll("button")[0];
+let h1 = document.querySelector("h1");
+
+// button.textContent += " Hola---";-
+console.log(button);
+
+button.addEventListener('click', changeColor);
+// button.onclick = changeColor;
+// button.addEventListener('click', () => {
+//     h1.textContent = " Hola";
+//     h1.style.color = "red";
+// });
+
+let emojiArray = [
+    "😐", "🤧", "😏", "👌", "😄", "🥳", "🤣", 
+    "🥰", "😘", "😍", "😋", "😎", "🤣", "😃", "😎",
+    "😚", "😑", "😪", "👲", 
+    "🧔", "🧥", "🧶", "🎞", "🥟", "🍠", "🍟", "🍿", "🍔", "🍕"
+]
+function changeColor(){
+    let emoji = emojiArray[Math.floor(Math.random() * emojiArray.length)]
+    h1.textContent = emoji;
+
+}
